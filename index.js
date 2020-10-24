50de3f9ff98a002312bed48b4bc7380fee401c08
Eduardo Piña <mrejpm@gmail.com>
2020-10-23 21:44:33 -0400

    [fix] database compatibility

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const salt = bcrypt.genSaltSync(saltRounds);
 require('dotenv').config();
-const db =knex({ client: 'pg', connection: process.env.DATABASE_URL}
-});
+const db =knex({ client: 'pg', connection: process.env.DATABASE_URL});
 
 
 const {pedirInventario} = require('./controllers/inventario');
