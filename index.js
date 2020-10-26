14307a127702b8f7597e4f70fa2b3461489a4a1f
Eduardo Piña <mrejpm@gmail.com>
2020-10-26 16:55:24 -0400

    [Add] cors package

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const knex = require('knex');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
