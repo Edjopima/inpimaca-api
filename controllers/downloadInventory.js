a63a5dc836033cb6301af843fdd2d0b9cafb8288
Eduardo Piña <mrejpm@gmail.com>
2023-07-03 13:26:48 -0500

    add export inventory to pdf

--- controllers/downloadInventory.js
+++ controllers/downloadInventory.js
@@ -1,6 +1,25 @@
+const XLSX = require("xlsx");
+
 const downloadInventory = async (req, res, db) => {
   const data = await db.select("*").from("inventory");
   console.log(data);
+
+  const workbook = XLSX.utils.book_new();
+  const worksheet = XLSX.utils.json_to_sheet(data);
+  XLSX.utils.book_append_sheet(workbook, worksheet, "Inventory");
+
+  // Guardar el archivo temporalmente en el servidor
+  const filePath = "temp/data.xlsx";
+  XLSX.writeFile(workbook, filePath, { bookType: "xlsx", type: "file" });
+
+  // Descargar el archivo
+  res.download(filePath, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      fs.unlinkSync(filePath);
+    }
+  });
 };
 
 module.exports = {
